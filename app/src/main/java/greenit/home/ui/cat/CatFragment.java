94b76c503fdebc4dccbcfbc3bcb1c246ec0a57d3
Łukasz Duda <lukasz.duda.pl@gmail.com>
2020-01-05 20:39:25 +0100

    Handle click

--- app/src/main/java/greenit/home/ui/cat/CatFragment.java
+++ app/src/main/java/greenit/home/ui/cat/CatFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import greenit.home.R;
 
-public class CatFragment extends Fragment {
+public class CatFragment extends Fragment implements View.OnClickListener {
 
     private CatViewModel catViewModel;
 
@@ -20,7 +24,14 @@ public class CatFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         catViewModel = ViewModelProviders.of(this).get(CatViewModel.class);
-        return inflater.inflate(R.layout.cat_fragment, container, false);
+        View root = inflater.inflate(R.layout.cat_fragment, container, false);
+        final Button pee = root.findViewById(R.id.pee);
+        pee.setOnClickListener(this);
+        return root;
     }
 
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(), "ok", Toast.LENGTH_SHORT).show();
+    }
 }
